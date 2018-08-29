0dc6bbc77e310ff56975a701faae36500039d180
qingmei2 <mq2553299@qq.com>
2018-08-29 17:24:37 +0800

    Update the Theme.

--- rhine/src/main/java/com/qingmei2/rhine/binding/ViewBinding.kt
+++ rhine/src/main/java/com/qingmei2/rhine/binding/ViewBinding.kt
@@ -1,29 +1,62 @@
 package com.qingmei2.rhine.binding
 
+import android.content.Context
 import android.databinding.BindingAdapter
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.jakewharton.rxbinding2.view.RxView
 import io.reactivex.functions.Consumer
 import java.util.concurrent.TimeUnit
+
 
 interface ViewClickConsumer : Consumer<View>
 
 const val DEFAULT_THROTTLE_TIME = 500L
 
+/**
+ * [View]是否可见
+ *
+ * @param visible 值为true时可见
+ */
 @BindingAdapter("bind_visible")
 fun setVisible(view: View, visible: Boolean) {
     view.visibility = if (visible) View.VISIBLE else View.GONE
 }
 
+/**
+ * [View]长按事件
+ *
+ * @param consumer 事件消费者
+ */
 @BindingAdapter("bind_long_click")
 fun setOnLongClickEvent(view: View, consumer: ViewClickConsumer) {
     RxView.longClicks(view)
             .subscribe { consumer.accept(view) }
 }
 
+/**
+ * [View]防抖动点击事件
+ *
+ * @param consumer 点击事件消费者
+ * @param time 防抖动时间间隔，单位ms
+ */
 @BindingAdapter("bind_first_click", "bind_throttle_time")
 fun setOnClickEvent(view: View, consumer: ViewClickConsumer, time: Long?) {
     RxView.clicks(view)
             .throttleFirst(time ?: DEFAULT_THROTTLE_TIME, TimeUnit.MILLISECONDS)
             .subscribe { consumer.accept(view) }
+}
+
+/**
+ * [View]被点击时,关闭输入框
+ *
+ * @param closed 当值为true时，启动该功能
+ */
+@BindingAdapter("onClickCloseSoftInput")
+fun closeSoftInputWhenClick(view: View, closed: Boolean) {
+    RxView.clicks(view)
+            .subscribe {
+                val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(view.windowToken, 0)
+            }
 }
