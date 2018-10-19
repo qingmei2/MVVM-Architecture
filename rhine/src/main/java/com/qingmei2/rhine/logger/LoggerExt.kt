bcb5b5c80f2a7c58329af4f6e684325bd3fcac04
qingmei2 <mq2553299@qq.com>
2018-10-19 18:25:54 +0800

    pref: add the Either class and log extensions.

--- rhine/src/main/java/com/qingmei2/rhine/logger/LoggerExt.kt
+++ rhine/src/main/java/com/qingmei2/rhine/logger/LoggerExt.kt
@@ -8,4 +8,16 @@ fun Application.initLogger(isDebug: Boolean = true) {
         Timber.plant(Timber.DebugTree())
     else
         Timber.plant(CrashReportingTree())
+
+    log { "initLogger successfully, isDebug = $isDebug" }
 }
+
+inline fun log(supplier: () -> String) = logd(supplier)
+
+inline fun logd(supplier: () -> String) = Timber.d(supplier())
+
+inline fun logi(supplier: () -> String) = Timber.i(supplier())
+
+inline fun logw(supplier: () -> String) = Timber.w(supplier())
+
+inline fun loge(supplier: () -> String) = Timber.e(supplier())
