9c8f9e5298b371a11ed88e27ddb00b54fe8b8a31
qingmei2 <mq2553299@gmail.com>
2019-01-01 15:45:42 +0800

    add land-mode usage code for mvvm.

--- app/src/main/java/com/qingmei2/sample/ui/main/MainFragment.kt
+++ app/src/main/java/com/qingmei2/sample/ui/main/MainFragment.kt
@@ -2,6 +2,7 @@ package com.qingmei2.sample.ui.main
 
 import android.annotation.SuppressLint
 import android.view.MenuItem
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.qingmei2.rhine.base.view.fragment.BaseFragment
@@ -15,6 +16,7 @@ import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
 
+@Suppress("PLUGIN_WARNING")
 @SuppressLint("CheckResult")
 class MainFragment : BaseFragment<FragmentMainBinding>() {
 
@@ -31,13 +33,15 @@ class MainFragment : BaseFragment<FragmentMainBinding>() {
     fun initFragments(): List<Fragment> =
             listOf(HomeFragment(), ReposFragment(), ProfileFragment())
 
-    fun onPageSelectChanged(index: Int) {
-        childFragmentManager
+    // port-mode only
+    fun onPageSelectChangedPort(index: Int) {
         for (position in 0..index) {
-            navigation.menu.getItem(position).isChecked = index == position
+            if (navigation.visibility == View.VISIBLE)
+                navigation.menu.getItem(position).isChecked = index == position
         }
     }
 
+    // port-mode only
     fun onBottomNavigationSelectChanged(menuItem: MenuItem) {
         when (menuItem.itemId) {
             R.id.nav_home -> {
@@ -50,5 +54,19 @@ class MainFragment : BaseFragment<FragmentMainBinding>() {
                 viewPager.currentItem = 2
             }
         }
+    }
+
+    // land-mode only
+    fun onPageSelectChangedLand(index: Int) {
+        if (viewPager.currentItem != index) {
+            viewPager.currentItem = index
+            closeFabMenuLand()
+        }
+    }
+
+    // land-mode only
+    private fun closeFabMenuLand() {
+        if (fabMenu != null && fabMenu.isExpanded)
+            fabMenu.toggle()
     }
 }
