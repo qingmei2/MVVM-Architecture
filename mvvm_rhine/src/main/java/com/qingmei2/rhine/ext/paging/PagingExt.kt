53840d3599f758e2a36a107012687b0e76a22e8c
qingmei2 <mq2553299@gmail.com>
2019-04-10 17:17:54 +0800

    remove Paging object class, it is worse usage for paging library.

--- mvvm_rhine/src/main/java/com/qingmei2/rhine/ext/paging/PagingExt.kt
+++ mvvm_rhine/src/main/java/com/qingmei2/rhine/ext/paging/PagingExt.kt
@@ -8,56 +8,13 @@ import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
 import io.reactivex.Scheduler
 
-object Paging {
-
-    fun <T> buildReactiveStream(
-            intPageKeyedDataSource: IntPageKeyedDataSource<T>,
-            enablePlaceholders: Boolean = false,
-            pageSize: Int = PAGING_DEFAULT_PAGE_SIZE,
-            initialLoadSizeHint: Int = PAGING_DEFAULT_INITIAL_LOAD_SIZE_HINT,
-            prefetchDistance: Int = PAGING_DEFAULT_PREFETCH_DISTANCE
-    ): Flowable<PagedList<T>> =
-            buildRxJavaPagedList(
-                    dataSourceFactory = intPageKeyedDataSource(dataSource = intPageKeyedDataSource),
-                    enablePlaceholders = enablePlaceholders,
-                    pageSize = pageSize,
-                    initialLoadSizeHint = initialLoadSizeHint,
-                    prefetchDistance = prefetchDistance
-            )
-
-    private fun <T> intPageKeyedDataSource(
-            dataSource: IntPageKeyedDataSource<T>
-    ): DataSource.Factory<Int, T> =
-            object : DataSource.Factory<Int, T>() {
-
-                override fun create(): DataSource<Int, T> = dataSource
-            }
-
-    private fun <T> buildRxJavaPagedList(
-            dataSourceFactory: DataSource.Factory<Int, T>,
-            enablePlaceholders: Boolean,
-            pageSize: Int,
-            initialLoadSizeHint: Int,
-            prefetchDistance: Int
-    ): Flowable<PagedList<T>> =
-            RxPagedListBuilder<Int, T>(
-                    dataSourceFactory,
-                    PagedList.Config
-                            .Builder()
-                            .setInitialLoadSizeHint(initialLoadSizeHint)
-                            .setPageSize(pageSize)
-                            .setPrefetchDistance(prefetchDistance)
-                            .setEnablePlaceholders(enablePlaceholders)
-                            .build()
-            ).buildFlowable(BackpressureStrategy.LATEST)
-}
-
 fun <Key, Value> DataSource.Factory<Key, Value>.toRxPagedList(
+        config: PagedList.Config? = null,
         boundaryCallback: PagedList.BoundaryCallback<Value>? = null,
         fetchSchedulers: Scheduler = RxSchedulers.io
 ): Flowable<PagedList<Value>> {
     return RxPagedListBuilder<Key, Value>(
-            this, PagedList.Config.Builder()
+            this, config ?: PagedList.Config.Builder()
             .setInitialLoadSizeHint(PAGING_DEFAULT_INITIAL_LOAD_SIZE_HINT)
             .setPageSize(PAGING_DEFAULT_PAGE_SIZE)
             .setPrefetchDistance(PAGING_DEFAULT_PREFETCH_DISTANCE)
