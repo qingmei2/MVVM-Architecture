fe0a7e14edbc55e814f493c972e67d860165a22b
qingmei2 <mq2553299@gmail.com>
2019-06-06 15:54:34 +0800

    remove LiveData dependency.

--- mvvm_rhine/src/main/java/com/qingmei2/rhine/ext/arrow/OptionExt.kt
+++ mvvm_rhine/src/main/java/com/qingmei2/rhine/ext/arrow/OptionExt.kt
@@ -4,6 +4,3 @@ import arrow.core.Option
 
 inline fun <T> Option<T>.whenNotNull(consumer: (T) -> Unit) =
         fold({}, consumer)
-
-inline fun <T> Option<T>.whenEmpty(action: () -> Unit) =
-        fold(action, {})
